e95b3d76e0b71d0ba609d5755283d6e4c868d58f
leo-dillon <jeannotegui@gmail.com>
2025-04-11 09:48:40 -0300

    subiendo enrutado a .nev

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import routerApi from './routes/index.js'
+import dotenv from 'dotenv'
 
-const PORT = 5000
+dotenv.config()
+
 const app = express()
+const PORT = process.env.PORT
+
 app.use(express.json())
+
 app.get('/',(request,response)=>{
     console.log('Ruta Raiz');
     response.send('Home')
     })
-routerApi(app)
+
+    routerApi(app)
 
 app.listen(PORT, () => {
     console.log(`El servidor correra en http://localhost:${PORT}`)
