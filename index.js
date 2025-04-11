7a133477b7c5890fb7e081e616c8589e211711b0
leo-dillon <jeannotegui@gmail.com>
2025-04-11 12:56:17 -0300

    Generando una pequeña vista

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(express.static('public'))
 
 app.get('/',(request,response)=>{
-    console.log('Ruta Raiz');
     response.send('Home')
-    })
+})
 
-    routerApi(app)
+routerApi(app)
 
 app.listen(PORT, () => {
     console.log(`El servidor correra en http://localhost:${PORT}`)
