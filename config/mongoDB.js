e7653192372a260cb878333ec88db26f31880547
Leonardo Dillon <jeannotegui@gmail.com>
2025-04-20 21:21:59 -0300

    Arreglando el mongo Config

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 
 const MDB_USER = process.env.MDB_USER
-const MDB_PASSWORD = process.env.MDB_PASSWORD
+const MDB_USER_PASSWORD = process.env.MDB_USER_PASSWORD
 const MDB_SERVER = process.env.MDB_SERVER
 
-const uri = `mongodb+srv://${MDB_USER}:${MDB_PASSWORD}@${MDB_SERVER}/?retryWrites=true&w=majority&appName=Cluster0`
+console.log(MDB_USER, MDB_SERVER, MDB_USER_PASSWORD)
+
+const uri = `mongodb+srv://${MDB_USER}:${MDB_USER_PASSWORD}@${MDB_SERVER}/?retryWrites=true&w=majority&appName=Cluster0`
 
 export const connectMongoose = async () => {
     try {
